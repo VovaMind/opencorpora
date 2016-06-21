e9bf061e01b5f9c0163088db00788c004cb2bda4
VovaMind <VovaMin@tut.by>
2016-06-21 14:21:02 +0300

    just update main.py

--- Dialog2016_track/main.py
+++ Dialog2016_track/main.py
@@ -3,16 +3,23 @@
 main-файл для извлечения признаков
 '''
 
-from MarkupCorpus import *
+from FeaturesExtractor import *
+import time
 
 def extractFeatures():
 	'''
 	Извлекаем признаки
 	'''
-	inputDir = "C:\\development\\OpenCorpora\\FactExtAutoAssesst\\data\\factRuEval-2016-master\\factRuEval-2016-master\\devset"
-	corpus = MarkupCorpus(inputDir)
-	for doc in corpus:
-		pass
+	# Путь к ручной разметке
+	markupDir = "C:\\development\\OpenCorpora\\FactExtAutoAssesst\\data\\factRuEval-2016-master\\factRuEval-2016-master\\devset"
+	# Путь к результатам от участников
+	participantsDir = "C:\\development\\OpenCorpora\\FactExtAutoAssesst\\data\\factrueval2016"
+	# Извлекаем признаки
+	outputFileName = "C:\\development\\OpenCorpora\\FactExtAutoAssesst\\data\\MarkupData.csv"
+	FeaturesExtractor.extractMarkupData_track1(markupDir, participantsDir, outputFileName)
 
 if __name__ == '__main__':
+	beginTime = time.time()
 	extractFeatures()
+	endTime = time.time()
+	print("Working time: " + str(endTime-beginTime))
