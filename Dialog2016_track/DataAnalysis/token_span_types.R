484fee3f06a3c447e93998192788bf6c9757de2a
VovaMind <VovaMin@tut.by>
2017-01-12 20:38:08 +0100

    typo fix

--- Dialog2016_track/DataAnalysis/token_span_types.R
+++ Dialog2016_track/DataAnalysis/token_span_types.R
@@ -13,7 +13,7 @@ filteredData <- data[ , !(names(data) %in% drops)]
 data$is_test <- as.logical(data$is_test)
 
 rare_output <- subset(as.data.frame(table(data$token_span_types, dnn = ('Value'))), Freq < 5)$Value
-is_rare_output = data$token_objects %in% rare_output
+is_rare_output = data$token_span_types %in% rare_output
 data[is_rare_output,]$is_test <- F
 
 boost_model <- randomForest(token_span_types~., data=filteredData[!data$is_test,], mfinal=10)
