1df4404afdb7bd9a6214a66b90fabfbabf547d73
VovaMind <VovaMin@tut.by>
2017-06-05 00:23:06 +0200

    big refactor: remove old stuff

--- Dialog2016_track/FeaturesExtractor/id_generator.py
+++
@@ -1,9 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-
-class IdGenerator:
-	''' Генерация последовательных id'ков. '''
-	current_id = 0
-	@staticmethod
-	def get():
-		IdGenerator.current_id += 1
-		return IdGenerator.current_id
